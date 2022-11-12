8845f1fc1f823bd29f58c291d0d57b16b61ff4e5
Chau Phuoc <Chauphuoc1996@gmail.com>
2022-11-12 10:43:09 +0700

    update

--- ArrayFunction/src/main/java/com/ArrayFunction/Main.java
+++ ArrayFunction/src/main/java/com/ArrayFunction/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        countWord();
+        creatArray();
 
     }
 //    Đếm số lần xuất hiện của ký tự trong chuỗi
