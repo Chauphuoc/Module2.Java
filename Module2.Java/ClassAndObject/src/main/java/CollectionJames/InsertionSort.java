b6dc43e7f7874065e758a35ffce7ccdc29d102af
Chau Phuoc <Chauphuoc1996@gmail.com>
2022-12-06 10:49:17 +0700

    update

--- Module2.Java/ClassAndObject/src/main/java/CollectionJames/InsertionSort.java
+++ Module2.Java/ClassAndObject/src/main/java/CollectionJames/InsertionSort.java
@@ -4,18 +4,33 @@ public class InsertionSort {
     static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
     public static void insertionSort (int[] list){
         for (int i = 1; i< list.length;i++){
-            int temp = list[i];
             int j = i-1;
+            int temp = list[i];
             while (j>=0&&list[j]>temp){
-                list[j+1]=list[j];
+                list[j+1] = list[j];
                 j--;
             }
             list[j+1]=temp;
         }
     }
 
+    public static void selectionSort (int [] list){
+        for (int i = 0; i < list.length-1;i++){
+            int minIndex = i;
+            for (int j = i+1;j< list.length;j++){
+                if (list[minIndex]>list[j]){
+                    minIndex = j;
+                }
+            }
+            int temp = list[i];
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
+        }
+    }
+
     public static void main(String[] args) {
-        insertionSort(list);
+//        insertionSort(list);
+        selectionSort(list);
         for (int i : list){
             System.out.print(i+ " ");
         }
