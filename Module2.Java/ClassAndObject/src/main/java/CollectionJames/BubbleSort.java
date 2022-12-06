48ea85c439a83cb27ac977eea1750f7cfed8cb63
Chau Phuoc <Chauphuoc1996@gmail.com>
2022-12-06 08:51:29 +0700

    update

--- Module2.Java/ClassAndObject/src/main/java/CollectionJames/BubbleSort.java
+++ Module2.Java/ClassAndObject/src/main/java/CollectionJames/BubbleSort.java
@@ -3,15 +3,19 @@ package CollectionJames;
 public class BubbleSort {
     static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
     public void bubbleSort(int[] list){
-        for (int i = 0; i< list.length-1;i++){
-            for (int j = i; j <list.length;j++){
-                if (list[i]>list[j]){
+        boolean swapSomething = true;
+        while (swapSomething){
+            swapSomething = false;
+            for (int i = 0; i< list.length-1; i++){
+                if (list[i]>list[i+1]){
                     int temp = list[i];
-                    list[i]=list[j];
-                    list[j]=temp;
+                    list[i]=list[i+1];
+                    list[i+1]=temp;
+                    swapSomething = true;
                 }
             }
         }
+
     }
 
     public static void main(String[] args) {
